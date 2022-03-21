6d8fc47e49a1c2fa39398d6f906a6a92a884a90c
sakrock007 <saket@hashstack.finance>
2022-03-21 11:49:43 +0530

    added addDeposit, addLoan, updateWhitellisted etc.

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,5 +1,6 @@
 const { GraphQLScalarType, Kind } = require('graphql');
 const db = require('../database/db')
+const uuid = require('uuid')
 
 const resolverMap = {
   Date: new GraphQLScalarType({
@@ -24,20 +25,103 @@ exports.resolvers = {
     Date: resolverMap,
 
     Query: {
-        account : (_, {address}) => {
+        getAccountDetailsByAddress : (parent, {address}) => {
           return db.select('*').from('accounts').where({address:address}).first();
         },
-        accounts: () => {
+        getAllAccounts: () => {
           return db.select('*').from('accounts');
-        }
+        },
+        getAllDepositByAccountId : (parent, {account_id}) => {
+          return db.select('*').from('deposits').where({account_id:account_id});
+        },
+        getAllLoanByAccountId : (_, {account_id}) => {
+          return db.select('*').from('loans').where({account_id:account_id});
+        },
     },
 
     Mutation : {
-        createAccount : async (_, args) => {
-          const id = uuid()
-          const account = { id, ...args }
-          await knex('accounts').insert(account)
+        addAccount : async (parent, args) => {
+          const id = uuid.v4()
+          const whitelist_status_id = 2 
+          console.log(args)
+          const account = { id, whitelist_status_id, ...args }
+          await db.from('accounts').insert(account)
           return account
+        },
+        addDeposit : async (parent, args) => {
+          const accountId = args.account_id;
+          const depositCommitment = args.commitment;
+          const depositMarket = args.market;
+          const depositAmount = args.amount;
+          //find the deposit-id, if deposit already exist
+          const existingDeposit = await db.select('*').from('deposits').where({account_id:accountId ,commitment:depositCommitment, market:depositMarket}).first();
+          console.log(existingDeposit)
+          //If deposit already exists
+          if(existingDeposit){
+            current_net_amount = existingDeposit.net_amount
+            updated_net_amount = current_net_amount*1 + depositAmount*1
+
+            // check if acquired yield needs to be updated
+            // current_net_acquired_yield = db.select('net_amount').from('deposits').where({id:existingDepositId})
+            // updated_net_acquired_yield = current_net_amount + args.amount
+            console.log("UPDATING EXISTING DEPOSIT")
+            await db.from('deposits').where({id:existingDeposit.id})
+            .update({
+              net_amount: updated_net_amount,
+              updated_at: new Date()
+            })
+            return await db.select('*').from('deposits').where({id:existingDeposit.id}).first();
+          }
+          //else if deposit doesn't exists 
+          else{
+            const deposit = { 
+              id: uuid.v4(),
+              account_id: accountId,
+              commitment: depositCommitment,
+              market: depositMarket,
+              net_amount: depositAmount,
+              net_accrued_yield:0,
+              created_at: new Date(),
+              updated_at: new Date()
+            }
+            console.log("CREATING NEW DEPOSIT")
+            await db.from('deposits').insert(deposit)
+            return deposit;
+          }
+        },
+        addLoan : async (parent, args) => {    
+          
+          const loanData = {
+            id: uuid.v4(),
+            loan_market: args.loan_amount,
+            loan_amount: args.loan_amount,
+            collateral_market: args.collateral_market,
+            collateral_amount: args.collateral_amount,
+            commitment: args.commitment,
+            cdr: args.cdr,
+            debt_category: args.debt_category,
+            current_amount: args.current_amount,
+            current_market: args.current_market,
+            is_swapped: false,
+            loan_status_id: 2,
+            account_id: args.account_id,
+            created_at: new Date(),
+            updated_at: new Date()
+          }
+
+          await db.from('loans').insert(loanData)
+
+          return loanData
+        },
+        updateWhitelistStatus : async (parent, args) => {    
+          await db.from('accounts').where({id:args.account_id})
+            .update({
+              whitelist_status_id: args.whitelist_status_id,
+              updated_at: new Date()
+            })
+
+          //returning the updated whitelist status  
+          return await db.select('*').from('accounts').where({id:args.account_id}).first()
         }
     }
   };
--- src/typedefs/index.js
+++ src/typedefs/index.js
@@ -15,15 +15,64 @@ exports.typeDefs = gql`
     updated_at: Date
   }
 
+  type Deposit {
+    id: ID
+    account_id: ID
+    commitment: String
+    market: String
+    net_amount: Float
+    net_accrued_yield: Float
+    created_at: Date
+    updated_at: Date
+  }
+
+  type Loan {
+      id: ID
+      loan_market: String
+      loan_amount: Float
+      collateral_market: String
+      collateral_amount: Float
+      commitment: String
+      cdr: Float
+      debt_category: Int
+      current_amount: Float
+      current_market: String
+      is_swapped: Boolean
+      loan_status_id: String
+      account_id: ID
+      created_at: Date
+      updated_at: Date
+  }
+
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "accounts" query returns an array of zero or more Account (defined above).
   type Query {
-    accounts: [Account]
-    account(address: String!): Account
+    getAllAccounts: [Account]
+    getAccountDetailsByAddress(address: String!): Account
+    getAllDepositByAccountId(account_id: ID!): [Deposit]
+    getAllLoanByAccountId(account_id: ID!): [Loan]
   }
 
   type Mutation {
-    createAccount(address: String!, whitelist_status_id: Int!,created_at: Date!,updated_at: Date!): Account!
+    addAccount(address: String!, created_at: Date!, updated_at: Date!): Account!
+
+    addDeposit(account_id: ID!, commitment: String!, market: String!, amount: Float!): Deposit!
+
+    addLoan(loan_market: String!, loan_amount: Float!, collateral_market: String!, collateral_amount: Float!, commitment: String!, cdr: Float!, debt_category: Int!, current_amount: Float!, current_market: String!, account_id: ID!): Loan!
+
+    updateWhitelistStatus(account_id: ID!, whitelist_status_id: Int!): Account
   }
 `;
+
+/*
+---------------------------WHITELIST STATUS--------------------------- 
+2 - "WHITELIST_NOT_REQUESTED"
+10 - "WHITELIST_REQUESTED"
+18 - "WHITELISTED"
+
+---------------------------LOAN STATUS --------------------------------
+2- ""
+10-""
+18-""
+*/
