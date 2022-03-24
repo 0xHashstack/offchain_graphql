5860a51eac3888a229881fd3ac150d5a832232bd
sakrock007 <saket@hashstack.finance>
2022-03-24 17:05:51 +0530

    jest tests added for getAccountByAddress

--- src/tests/graphqlTestCall.js
+++ src/tests/graphqlTestCall.js
@@ -1,7 +1,7 @@
 import { graphql } from "graphql";
 import { makeExecutableSchema } from "graphql-tools";
 import { typeDefs } from "../typedefs/index";
-import { resolvers } from "../resolvers";
+import { resolvers } from "../resolvers/index";
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
--- src/tests/resolver.test.js
+++ src/tests/resolver.test.js
@@ -1,27 +1,32 @@
-const { Connection } = require("typeorm");
-import { graphqlTestCall } from "./graphqlTestCall";
-import { createTestConn } from "./createTestConn";
-const querygetAccountDetailsByAddressData = 
-    `query Query($address: String!) {
-        getAccountDetailsByAddress(address: $address) {
-          id
+const { graphqlTestCall } = require( "./graphqlTestCall");
+
+const getAccountDetailsByAddressQuery = `
+  query getAccountDetailsByAddress($address: String!) {
+    getAccountDetailsByAddress(address: $address) {
+      id,
+      address,
+      whitelist_status_id,
+      user_role,
+    }
+  }
+`;
+
+
+describe("resolvers", () => {
+  it("getAccountDetailsByAddress", async () => {
+    const response = await graphqlTestCall(getAccountDetailsByAddressQuery, { address: "0x0000000000000000000000DUMMY_1",});
+    console.log(response);
+    
+    expect(response).toEqual({
+      data: {
+        getAccountDetailsByAddress: {
+          id: "f554c8f6-06e6-4386-88b9-59047adb6365",
+          address: "0x0000000000000000000000DUMMY_1",
+          whitelist_status_id: 2,
+          user_role: "DUMMY_USER"
         }
       }
-}`;
-
-let conn;
-
-beforeAll(async () => {
-    conn = await createTestConn();
+    });
+    
   });
-  
-  afterAll(async () => {
-    await conn.close();
-  });
-
-  describe("resolvers", () => {
-    it("getAccountDetailsByAddress", async () => {
-
-
-    })
-})
+});
